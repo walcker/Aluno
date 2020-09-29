07a02472342883e5f75712b852806bd8de0c680f
Walcker Gomes <walckergomes@gmail.com>
2020-09-29 15:47:09 +0000

    Versão 1.1

--- main.cpp
+++ main.cpp
@@ -11,6 +11,15 @@ int main() {
   novo.setEmail("fortao@escolinha.com.br");
   novo.setTelefone("94509-3456");
 
+  Aluno* outro = new Aluno();
+  outro->setNome("Marilia Mendonça");
+  outro->setCpf("34254235345-56");
+  outro->setMatricula(54321);
+  outro->setEmail("Marilia@ufrn.br");
+  outro->setTelefone(novo.getTelefone());
+  
   cout << "Temos um novo aluno: " << novo.getNome() << endl;
+  cout << "Temos um novo aluno: " << outro->getNome() << endl;
+  cout << "Telefone: " << outro->getTelefone() << endl;
   return 0;
 }
